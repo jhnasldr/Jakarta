6f3794a583986f575d849743d169dd13868b356d
Morten Sandgrav <morten@sandgrav.se>
2024-04-15 22:01:12 +0200

    Committed BookingRepository, BookingServiceInterface, BookingService and AdminController class

--- src/main/java/com/bravo/jakarta/repositories/BookingRepository.java
+++ src/main/java/com/bravo/jakarta/repositories/BookingRepository.java
@@ -1,7 +1,9 @@
 package com.bravo.jakarta.repositories;
 
+import com.bravo.jakarta.entities.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class BookingRepository {
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
--- src/main/java/com/bravo/jakarta/services/BookingServiceInterface.java
+++ src/main/java/com/bravo/jakarta/services/BookingServiceInterface.java
@@ -1,4 +1,12 @@
 package com.bravo.jakarta.services;
 
+import com.bravo.jakarta.entities.Booking;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public interface BookingServiceInterface {
+    List<Booking> getAllBookings();
+
+    void deleteBooking(Long id);
 }
