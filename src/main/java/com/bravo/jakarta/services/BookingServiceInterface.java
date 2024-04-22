ba28fe5f09f5eef9abbe754230834f3507a8937f
Fabio <fabianknopf.umea@gmail.com>
2024-04-22 22:19:43 +0200

    kÖR

--- src/main/java/com/bravo/jakarta/services/BookingServiceInterface.java
+++ src/main/java/com/bravo/jakarta/services/BookingServiceInterface.java
@@ -3,10 +3,13 @@ package com.bravo.jakarta.services;
 import com.bravo.jakarta.entities.Booking;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.awt.print.Book;
 import java.util.List;
 
 public interface BookingServiceInterface {
     List<Booking> getAllBookings();
 
     void deleteBooking(Long id);
+
+    void cancelBooking(Long id);
 }
